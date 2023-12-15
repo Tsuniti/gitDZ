c5283e1e0f3b4bd01f4ac898bf6a546fda83c9da
Tsuniti <tsuniti00@gmail.com>
2023-12-15 18:13:30 +0200

    adding Ship.cs

---
+++ vehicle/Ship.cs
@@ -0,0 +1,27 @@
+﻿namespace gitDZ.vehicle;
+
+internal class Ship : IVehicle
+{
+    public string Model { get; }
+    public string Owner { get; set; }
+    public double FuelAmmount { get; set; }
+    private bool EngineRunning { get; set; }
+    public void EngineTurnOn()
+    {
+        if (FuelAmmount > 0)
+            EngineRunning = true;
+    }
+
+    public void EngineTurnOff()
+    {
+        EngineRunning = false;
+    }
+
+    public void DriveOneKm()
+    {
+        if (EngineRunning && FuelAmmount >= 0.5)
+        {
+            FuelAmmount -= 0.5;
+        }
+    }
+}
