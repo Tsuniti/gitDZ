3bf01fdebb6ce5744d535022b0533e6e388753ac
Tsuniti <tsuniti00@gmail.com>
2023-12-15 14:00:51 +0200

    adding Car.cs

---
+++ vehicle/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Schema;
+
+namespace gitDZ.vehicle;
+
+public class Car : IVehicle
+{
+    public string Model { get; }
+    private string Owner { get; set; }
+    private double FuelAmmount { get; set; }
+    public bool IsEngineRunning { get; set; }
+
+    public Car(string model, string owner, double fuelAmmount)
+    {
+        Model = model;
+        Owner = owner;
+        FuelAmmount = fuelAmmount;
+        IsEngineRunning = false;
+    }
+
+    public void EngineTurnOn()
+    {
+        if (FuelAmmount > 0)
+            IsEngineRunning = true;
+    }
+
+    public void EngineTurnOff()
+    {
+        IsEngineRunning = false;
+    }
+}
