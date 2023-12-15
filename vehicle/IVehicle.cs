9ef075a9c35c6b00b838cb54ee304848b52b25fb
Tsuniti <tsuniti00@gmail.com>
2023-12-15 13:04:50 +0200

    updating IVehicle

--- vehicle/IVehicle.cs
+++ vehicle/IVehicle.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace gitDZ.vehicle;
 
-namespace gitDZ.vehicle
+public interface IVehicle
 {
-    internal interface IVehicle
-    {
-    }
+    void EngineTurnOn();
+    void EngineTurnOff();
 }
